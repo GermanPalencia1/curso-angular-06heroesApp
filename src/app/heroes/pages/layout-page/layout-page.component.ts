e6c1c33fe1b3d20f69293ec050164442955349bf
Germán <german.palencia@adentia.es>
2023-09-11 14:24:53 +0200

    Angular Material Components

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
+
 export class LayoutPageComponent {
+
+  public sidebarItems = [
+    {label: 'Listado', icon: 'label', url: './list'},
+    {label: 'Añadir', icon: 'add', url: './new-hero'},
+    {label: 'Buscar', icon: 'search', url: './search'},
+  ];
 
 }
